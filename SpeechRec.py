f8d58604ca00aaa4c9c74d10c0d59d6a62aee5da
matty0077 <Moquendo993@gmail.com>
2018-07-30 20:16:55 +0000

    Add files via upload

--- SpeechRec.py
+++ SpeechRec.py
@@ -4,7 +4,7 @@ import speech_recognition as sr
 def listen():                               #adjust sample rate and chunk size according to your mic
     print("listening...")                           #my usb 44100          #512
     r = sr.Recognizer()                             #my web mic 16000      #128
-    m=sr.Microphone(device_index = 2, sample_rate = 44100, chunk_size = 512)
+    m=sr.Microphone(device_index = 1, sample_rate = 44100, chunk_size = 512)
     with m as source:
         r.adjust_for_ambient_noise(source, duration=1) # listen for 1 second to calibrate the energy threshold for ambient noise levels
         r.energy_threshold = 500#1250#175#400
@@ -13,12 +13,11 @@ def listen():                               #adjust sample rate and chunk size a
         with open("/memory/sounds/records/microphone-results.wav", "wb") as f:
             f.write(audio.get_wav_data())#records data to be recognized later
         audio.pause_threshold = 0.65# the seconds of silence needed to stop listening
-        
-    #pick one by commenting out the other
-    a =r.recognize_sphinx(audio)
-    #a=r.recognize_google(audio)
 
     try:
+          #pick one by commenting out the other
+        #a =r.recognize_sphinx(audio)
+        a=r.recognize_google(audio)
         print(a)
     except sr.UnknownValueError:
         print("could not understand audio")
